0f57c7b0cb195aaf810496234954fa2725ee2194
Kortex <jfcontart@idemobi.com>
2020-09-22 09:28:50 +0200

    [INIT] with apple version

---
+++ NWDDataManager_Security.cs
@@ -0,0 +1,107 @@
+//=====================================================================================================================
+//
+//  ideMobi 2020©
+//  All rights reserved by ideMobi
+//
+//=====================================================================================================================
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+using System.Text;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+//=====================================================================================================================
+namespace NetWorkedData
+{
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public static partial class SQLite3
+    {
+#if UNITY_EDITOR
+        private const string DLL_NAME = "sqlcipher";
+#elif UNITY_STANDALONE
+        private const string DLL_NAME = "sqlcipher";
+#elif UNITY_ANDROID
+		private const string DLL_NAME = "sqlcipher";
+#elif UNITY_IOS
+		private const string DLL_NAME = "__Internal";
+#elif UNITY_TVOS
+		private const string DLL_NAME = "__Internal";
+#endif
+    }
+    public partial class NWDDataManager
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        const string KDBPrefix = "P"; // old used 'O' for 4.3.0 '' for ealier version
+        //-------------------------------------------------------------------------------------------------------------
+        public string GetVersion()
+        {
+            string rReturn = SQLite3.LibVersionNumber().ToString();
+            string rCipherVersion = " (sqlcipher -error-)";
+            if (EditorDatabaseLoaded == true)
+            {
+                IntPtr stmt = SQLite3.Prepare2(SQLiteEditorHandle, "PRAGMA cipher_version;");
+                while (SQLite3.Step(stmt) == SQLite3.Result.Row)
+                {
+                    rCipherVersion = " (sqlcipher " + SQLite3.ColumnString(stmt, 0) + ")";
+                }
+                SQLite3.Finalize(stmt);
+            }
+            return rReturn + rCipherVersion;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public bool IsSecure()
+        {
+            return true;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public string DatabaseAccountName()
+        {
+            return KDBPrefix + NWDAppConfiguration.SharedInstance().DatabasePrefix + NWD.K_DeviceDatabaseName;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public string DatabaseEditorName()
+        {
+            return KDBPrefix + NWD.K_EditorDatabaseName;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public string DatabaseBuildName()
+        {
+            return KDBPrefix + NWD.K_BuildDatabaseName;
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public void DatabaseEditorOpenKey(string tDatabasePathEditor)
+        {
+            if (IsSecure())
+            {
+                string tEditorPass = NWDAppConfiguration.SharedInstance().GetEditorPass();
+                SQLite3.Result trResultPassword = SQLite3.Key(SQLiteEditorHandle, tEditorPass, tEditorPass.Length);
+                if (trResultPassword != SQLite3.Result.OK)
+                {
+                    throw SQLiteException.New(trResultPassword, string.Format("Could not open database file with password: {0} ({1})", tDatabasePathEditor, trResultPassword));
+                }
+            }
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public void DatabaseAccountOpenKey(string tDatabasePathAccount, string sSurProtection)
+        {
+            if (IsSecure())
+            {
+                string tAccountPass = NWDAppConfiguration.SharedInstance().GetAccountPass(sSurProtection);
+                SQLite3.Result trResultPassword = SQLite3.Key(SQLiteDeviceHandle, tAccountPass, tAccountPass.Length);
+                if (trResultPassword != SQLite3.Result.OK)
+                {
+                    throw SQLiteException.New(trResultPassword, string.Format("Could not open database file with password: {0} ({1})", tDatabasePathAccount, trResultPassword));
+                }
+            }
+        }
+        //-------------------------------------------------------------------------------------------------------------
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+//=====================================================================================================================
---
+++ SQLCipher_4-4-0.md
@@ -0,0 +1,21 @@
+# Unity3D Editor
+| OS | Arch |Working |
+| --- | --- | --- |
+| macOS editor| x86_64 | ? |
+| Windows editor| ? | ? |
+| Linux editor | ? | ? |
+
+# Unity3D Player
+| OS | Arch | Working | test on |
+| --- | --- | --- | --- |
+| macOS | x86_64 | ? | iMac (Retina 4K, 21.5-inch, 2017) |
+| iOS | armv7 | ? | |
+| iOS | armv7s | ? | |
+| iOS | arm64 | ? | |
+| tvOS | ? | ? | |
+| Windows | ? | ? | |
+| Android | armeabi-v7a | ? | |
+| Android | arm64-v8a | ? | Asus ZenPad 3S 10 |
+| Android | x86 | ? | |
+| Android | x86_64 | ? | |
+| Linux | ? | ? | |
